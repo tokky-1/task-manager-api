104fbfd318ab592b1e306ea013f0cc5d46e06d8b
Toki <tokkyayoajayi@gmail.com>
2025-08-22 18:54:07 +0100

    Day 3: Implemented a postgress database hosted on supabase

--- model.py
+++ model.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel,Field
-
+from typing import Optional
 class TaskModel(BaseModel):
     ID:int
     Title: str
     Description: str = Field(None)
     Completed: bool = False
+
+class updateTaskModel(BaseModel): # when using model object is always a dictionary
+    Title : Optional[str] = Field(None) 
+    Description: Optional[str] = Field(None)
